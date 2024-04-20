02230b2f21056503dad0ab479cf1a4e4d62301e8
Shivanshi <shivanshityagi@icloud.com>
2024-04-20 14:16:56 +0700

    link to etherscan

--- src/app/[address]/contract-image.tsx
+++ src/app/[address]/contract-image.tsx
@@ -23,7 +23,7 @@ export const ContractImage = ({
     }
 
     return <div>
-        <h1 className="fixed top-0 left-0 text-4xl text-foreground">Contract {address}</h1>
+        <h1 className="fixed top-0 left-0 text-4xl text-foreground">Contract <a href={`https://etherscan.io/address/${address}`} target="_blank" rel="noopener noreferrer">{address}</a></h1>
         <HexImage hex={data} />
     </div> 
 }
